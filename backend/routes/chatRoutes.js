770c5c22be23368c264cac2ba403d6854ea67afe
anivartak <jainanivartakkhurai@gmail.com>
2026-09-20 07:56:13 +0530

    redeploy

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -10,7 +10,7 @@ router.get("/status", async (_req, res) => {
     data: result,
   });
 });
-// resc
+
 router.post("/", async (req, res) => {
   try {
     const { message, history = [], language = "en", isQuickAction = false } = req.body;
